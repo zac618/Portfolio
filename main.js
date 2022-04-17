83bdec26b0dfa567b70943108177f03b8408b172
Zac E <zacegleston@gmail.com>
2022-04-17 11:00:58 -0500

    Update 4-17-2022

---
+++ main.js
@@ -0,0 +1,18 @@
+let seeAbout = document.getElementById('about-me-heading');
+const aboutBtn = document.getElementById('about-button');
+let seeProjects = document.getElementById('project-heading');
+const projectsBtn = document.getElementById('projects-button');
+let seeContact = document.getElementById('connect-heading');
+const contactBtn = document.getElementById('contact-button');
+
+aboutBtn.addEventListener("click", function(){
+    seeAbout.scrollIntoView();
+});
+
+projectsBtn.addEventListener("click", function(){
+    seeProjects.scrollIntoView();
+})
+
+contactBtn.addEventListener("click", function() {
+    seeContact.scrollIntoView();
+})
