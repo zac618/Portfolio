c3bbb306a8745bc291bb82d41b19864b30ef18f3
Zac E <zacegleston@gmail.com>
2023-07-25 21:33:09 -0500

    Fixed Button Scroll Behavior

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-let seeAbout = document.getElementById('about-me-heading');
+let seeAbout = document.getElementById('about-me-item');
 const aboutBtn = document.getElementById('about-button');
-let seeProjects = document.getElementById('project-heading');
+let seeProjects = document.getElementById('project-item');
 const projectsBtn = document.getElementById('projects-button');
-let seeContact = document.getElementById('connect-heading');
+let seeContact = document.getElementById('contact-me-items');
 const contactBtn = document.getElementById('contact-button');
 
 aboutBtn.addEventListener("click", function(){
-    seeAbout.scrollIntoView();
+    seeAbout.scrollIntoView({block: "center"});
 });
 
 projectsBtn.addEventListener("click", function(){
-    seeProjects.scrollIntoView();
+    seeProjects.scrollIntoView({block: "center"});
 })
 
 contactBtn.addEventListener("click", function() {
-    seeContact.scrollIntoView();
+    seeContact.scrollIntoView({block: "center"});
 })
